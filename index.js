22cf48900ed7879b6c1dc9a022c519e12771e4cc
Valoeghese <valoeghesemods@gmail.com>
2022-08-11 15:57:11 +1200

    tokeniser

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 fs = require('fs');
 const LABEL_REGEX = /[A-z0-9 ]+/;
-const IDENTIFIER_REGEX = /[A-z0-9]+/;
-const IDENTIFIER_START_REGEX = /[A-z]/;
-const SAFE_MATHS_CHARS_REGEX = /[\+\-\*\/ 0-9\(\)!&|]+/;
 
 // This is the easiest way to do it I swear
 // Source: https://stackoverflow.com/questions/2878703/split-string-once-in-javascript
@@ -36,19 +33,84 @@ function exception(lineNum, msg) {
 	return "EXCEPTION at line " + lineNum + ":\n>> " + msg;
 }
 
-// safety expression validator
+const IDENTIFIER_START_REGEX = /[A-z]/;
+const IDENTIFIER_CHAR_REGEX = /[A-z0-9]/;
+const SPACE_OPERATORS_REGEX = /[\t \+\-\*\/!&|=]/;
+const BRACKETS_OPERATORS_REGEX = /[\(\)\+\-\*\/!&|=]/;
+const NUMBERS_REGEX = /[0-9]/;
+
+const IDENTIFIER_REGEX = /[A-z][A-z0-9]*/;
+const SAFE_MATHS_CHARS_REGEX = /[\+\-\*\/ 0-9\(\)!&|]+/;
+
+// expression translator to js
 // what are you talking about, unsafely using eval? who could ever
 // returns the expression to evaluate, transformed from the input
 // dont do this
-function translateExpression(expression) {
+function translateExpression(lnm, expression) {
 	// tokeniser
 	// yes this is needed to prevent spaghetti
 	// todo
+	let tokens = [];
+	let varIndex = -1; // for slicing
+	let stringMode = 0; // 1 = string, 2 = escape
+	let stringAccumulator = ""; // for accumulating strings
 
+	for (let i = 0; i < expression.length; i++) {
+		let c = expression[i]; // current char
 
-	// test the remaining 'mathematical' part of the expression
-	// is this necessary with a tokeniser
-	return SAFE_MATHS_CHARS_REGEX.test(mathsAccumulator);
+		if (stringMode) {
+			if (stringMode == 2) {
+				stringMode = 1;
+			}
+			else if (c == '"') {
+				tokens.push({"type": "STRING", "name": stringAccumulator});
+				stringAccumulator = "";
+				stringMode = 0;
+			}
+			else {
+				stringAccumulator += c;
+			}
+		}
+		else if (varIndex > -1) {
+			if (!IDENTIFIER_CHAR_REGEX.test(c)) {
+				if (SPACE_OPERATORS_REGEX.test(c)) {
+					tokens.push({"type": "VAR", "name": expression.slice(varIndex, i)});
+					varIndex = -1;
+					i--; // take another look at
+				}
+				else {
+					throw exception(lnm + 1, "Unexpected character \"" + c + '"');
+				}
+			}
+		}
+		else if (c == ' ' || c == '\t') {
+			// no-op
+		}
+		else {
+			if (IDENTIFIER_START_REGEX.test(c)) {
+				varIndex = i;
+			}
+			else if (BRACKETS_OPERATORS_REGEX.test(c)) {
+				tokens.push({"type": "OPERATOR", "operator": c});
+			}
+			else if (c == '"') {
+				stringMode = 1;
+			}
+			else {
+				throw exception(lnm + 1, "Unexpected character \"" + c + '"');
+			}
+		}
+	}
+
+	if (stringMode) {
+		throw exception(lnm + 1, "Unclosed string!");
+	}
+
+	if (varIndex > -1) {
+		tokens.push({"type": "VAR", "name": expression.slice(varIndex, expression.length)});
+	}
+
+	return tokens;
 }
 
 // Parameters
@@ -85,6 +147,7 @@ function decode(lnm, instruction, labels) {
 	}
 
 	splitInstr = instruction.splitOnce(" ");
+	expression = splitInstr[1].trim();
 
 	// yes these are case sensitive cope
 	// allow string and num mixing
@@ -93,13 +156,20 @@ function decode(lnm, instruction, labels) {
 		case "REM":
 			return null; // comment 2 electric boogaloo
 		case "PRINT":
-
+			if (expression == '') throw exception(lnm + 1, "PRINT requires an operand but none given!");
+			console.log(translateExpression(lnm, expression));
 			break;
 		case "INPUT":
+			if (expression == '') throw exception(lnm + 1, "INPUT requires an operand but none given!");
+			console.log(translateExpression(lnm, expression));
 			break;
 		case "GOTO":
+			if (expression == '') throw exception(lnm + 1, "GOTO requires an operand but none given!");
+			console.log(translateExpression(lnm, expression));
 			break;
 		case "IF":
+			if (expression == '') throw exception(lnm + 1, "IF requires an operand but none given!");
+			console.log(translateExpression(lnm, expression));
 			break;
 		case "ELSE":
 			break;
